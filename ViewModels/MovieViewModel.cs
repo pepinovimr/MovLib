354c49ccec6cd822cd79a79233d3c47749e3c41c
pepinovimr <pepino.vimr@seznam.cz>
2023-06-04 21:38:11 +0200

    Added ViewModels

---
+++ ViewModels/MovieViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MovLib.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovLib.ViewModels
+{
+    class MovieViewModel : BaseViewModel
+    {
+        private readonly Movie _movie;
+
+        public string Title => _movie.title;
+        public string Overview => _movie.overview;
+        public float VoteAverage => _movie.vote_average;
+        public float VoteCount => _movie.vote_count;
+
+        public MovieViewModel(Movie movie)
+        {
+            _movie = movie;
+        }
+    }
+}
--- ViewModels/ShowMoviesViewModel.cs
+++ ViewModels/ShowMoviesViewModel.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using MovLib.Data.Context;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace MovLib.ViewModels
 {
     internal class ShowMoviesViewModel : BaseViewModel
     {
+        private readonly MoviesDbContext _context = new();
 
+        private readonly ObservableCollection<MovieViewModel> _movies;
+
+        public IEnumerable<MovieViewModel> Movies => _movies;
+
+        private string _filterText;
+		public string FilterText
+		{
+			get
+			{
+				return _filterText;
+			}
+			set
+			{
+				_filterText = value;
+				OnPropertyChanged(nameof(FilterText));
+			}
+		}
+
+		public ICommand DeleteCommand { get; }
+        public ICommand ChangeCommand { get; }
+
+        public ShowMoviesViewModel()
+        {
+			_context.Movies.Load();
+			_movies = new();
+        }
     }
 }
